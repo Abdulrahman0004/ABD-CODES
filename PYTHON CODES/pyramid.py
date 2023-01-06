d7d0dff701c0a0394ac499fdf1cce33252c1346a
Abdulrahman0004 <113187862+Abdulrahman0004@users.noreply.github.com>
2023-01-06 19:54:03 +0530

    Add files via upload

---
+++ PYTHON CODES/pyramid.py
@@ -0,0 +1,9 @@
+row=int(input("Enter the Number of rows:"))
+for i in range(1,row+1):
+    for j in range(1,i+1):
+        print('*',end=' ')
+    print()
+for k in range(1,row+1):
+    for l in range(k,5):
+        print('*',end=' ')
+    print()
